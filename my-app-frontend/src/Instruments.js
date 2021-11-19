ea9b233147f7a726b46fd4b57715dac909fcef2b
Duke Norsworthy <duke.norsworthy@gmail.com>
2021-11-19 12:56:02 -0500

    instrument form initialized

--- my-app-frontend/src/Instruments.js
+++ my-app-frontend/src/Instruments.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Instruments() {
 
     const [instruments, setInstruments] = useState([])
+    const [error, setError] = useState("")
 
     useEffect(() => {
         fetch("http://localhost:9292/instruments")
@@ -13,12 +14,24 @@ export default function Instruments() {
         })
     }, [])
 
+    const handleDelete = (e) => {
+        setError("")
+        fetch(`http://localhost:9292/musicians/${e.target.id}`, {
+            method: "DELETE",
+            headers: {'Content-Type': 'application/json'},
+            body: null
+        }).then(() => setError("Delete successful"))
+    }
+    
     const instrumentElements = instruments.map((i) => {
         return <li key={i.id}>
             {i.name}
-            <button>Delete</button>
+            <> </>
+            <button onClick={handleDelete}>Delete</button>
         </li>
     })
+
+
 
     return (
         <div>
@@ -26,6 +39,8 @@ export default function Instruments() {
                 {instrumentElements}
             </ul>
             <Link to="/" className="back-button">Back</Link>
+            <> </>
+            {error}
         </div>
     )
 }
